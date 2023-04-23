f5801573872c829a1af754cd8d8d0038370edd96
bad80 <>
2023-04-23 07:05:45 +0000

    Updates

---
+++ _next/static/B8ARubuDOqerM_7J8KgHf/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/f71d2254694db3e4.css","static/chunks/pages/index-23e93e8f7df49fe8.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
